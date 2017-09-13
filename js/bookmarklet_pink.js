4466c767631c67f08688251e0efd5481ae4da247
jtran2 <jtran2@cca.edu>
2017-09-12 18:38:04 -0700

    yellow

--- js/bookmarklet_pink.js
+++ js/bookmarklet_pink.js
@@ -32,7 +32,7 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
 
-        javascript:void(document.body.style.background=pink);
+        javascript:void(document.body.style.background="yellow");
 
     })();
 
