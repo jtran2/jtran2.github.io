116f2b7d4a8a9b83aab8ec0c9d671eb1528fca65
jtran2 <jtran2@cca.edu>
2017-09-19 23:35:43 -0700

    project 1 folder

---
+++ proj1/bookmarklet.js
@@ -0,0 +1,56 @@
+javascript: function Gosling(i, g) {
+    this.ratio = i, this.imageurl = g
+}
+
+function Randomize(i) {
+    return Math.floor(Math.random() * i.length)
+}
+
+function imageRatio(i) {
+    var g = i.height / i.width;
+    return g > 1 ? "vertical" : 1 === g ? "square" : 1 > g ? "horizontal" : void 0
+}
+var getGosling = {
+        init: function(i) {
+            this.myGosling = i
+        },
+        horizontal: function() {
+            return this.myGosling.filter(function(i) {
+                return "horizontal" === i.ratio
+            })
+        },
+        vertical: function() {
+            return this.myGosling.filter(function(i) {
+                return "vertical" === i.ratio
+            })
+        },
+        square: function() {
+            return this.myGosling.filter(function(i) {
+                return "square" === i.ratio
+            })
+        }
+    },
+    myGosling = [
+    new Gosling("horizontal", "https://jtran2.github.io/P1/animals/horizontal1.jpg"), 
+    new Gosling("horizontal", "https://jtran2.github.io/P1/animals/horizontal2.jpg"), 
+    new Gosling("horizontal", "https://jtran2.github.io/P1/animals/horizontal3.jpg"), 
+    new Gosling("horizontal", "https://jtran2.github.io/P1/animals/horizontal4.jpg"), 
+
+    new Gosling("vertical", "https://jtran2.github.io/P1/animals/vertical1.jpg"), 
+    new Gosling("vertical", "https://jtran2.github.io/P1/animals/vertical2.jpg"), 
+    new Gosling("vertical", "https://jtran2.github.io/P1/animals/vertical3.jpg"), 
+ 
+    new Gosling("square", "https://jtran2.github.io/P1/animals/square1.jpg"), 
+    new Gosling("square", "https://jtran2.github.io/P1/animals/square2.jpg"), 
+    new Gosling("square", "https://jtran2.github.io/P1/animals/square3.jpg"), 
+];
+
+! function(i) {
+    getGosling.init(myGosling);
+    for (var g = i.getElementsByTagName("img"), o = g.length, n = 0; o > n; n++) {
+        var l = imageRatio(g[n]),
+            t = Randomize(getGosling[l]()),
+            e = getGosling[l]()[t];
+        g[n].src = e.imageurl
+    }
+}(document);
