2fc6cab6c446c9f2e81491b310d47107d2a44249
jtran2 <jtran2@cca.edu>
2017-09-25 11:05:47 -0700

    word repalce add

--- proj1/bookmarklet.js
+++ proj1/bookmarklet.js
@@ -32,7 +32,7 @@ function initMyBookmarklet(){
 
         //YOUR CODE GOES HERE!
 
-        javascript:void(document.body.innerHTML = document.body.innerHTML.replace(/Trump/g, 'Drumpf').replace(/North Korea/g, 'North Dogland'));
+        javascript:void(document.body.innerHTML = document.body.innerHTML.replace(/Trump/g, 'Drumpf').replace(/Korea/g, 'Dogland'));
 
     })();
 
